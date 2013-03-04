f42a8234d77623336532c4b5e979884d1fe7f44b
Barry Melton <barry.melton@gmail.com>
2013-03-03 21:22:04 -0800

    Added management command to calculate scores.  ./manage.py calculate_scores will update all the scores in Articles.  Should be cronnable.

---
+++ management/commands/calculate_scores.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from djig.models import Article
+
+class Command(BaseCommand):
+    help = "Loops through articles and calculates score."
+
+    def handle(self, *args, **options):
+        articles = Article.objects.all()
+        for article in articles:
+            article.calculate_score()
+        self.stdout.write("Finised calculating scores.")
