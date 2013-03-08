c3a29d0bf669b3e9364b677be0085f0684897d99
Barry Melton <barry.melton@gmail.com>
2013-03-08 15:32:55 -0800

    Set up logging for cron tasks.

--- djig/management/commands/calculate_scores.py
+++ djig/management/commands/calculate_scores.py
@@ -1,12 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
+import logging
+logging.basicConfig(filename='calculate_scores.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 from djig.models import Article
+from datetime import datetime
 
 class Command(BaseCommand):
     help = "Loops through articles and calculates score."
 
     def handle(self, *args, **options):
+        logging.debug("Calculating scores at %s" % datetime.now())
         articles = Article.objects.all()
         for article in articles:
             article.calculate_score()
-        self.stdout.write("Finished calculating scores.")
+               logging.debug("Calculated score for %s" % (article.title))
+        logging.debug("Finished calculating scores")
+        self.stdout.write("Finished calculating scores at %s." % datetime.now())
