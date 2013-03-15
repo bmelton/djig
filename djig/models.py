81fafcd7bdfc9203c9180d0b899ed3211ac5d496
Barry Melton <barry.melton@gmail.com>
2013-03-15 07:29:04 -0700

    Made search work in good-old 'everybody' mode.  This fits in with the framework, but makes it easy for people to avoid all the integration work.

--- djig/models.py
+++ djig/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.forms import ModelForm
 import datetime
 from uuslug import uuslug 
 import times
 from django.utils.encoding import smart_unicode
 from math import ceil
+from guardian.shortcuts import assign, get_perms, get_groups_with_perms
+
+everybody_group = Group.objects.get(name="Everybody")
 
 class Category(models.Model):
     title                   = models.CharField(max_length=100)
@@ -13,7 +16,7 @@ class Category(models.Model):
     image                   = models.URLField(null=True, blank=True)
     description             = models.TextField()
 
-    class Meta():
+    class Meta:
         verbose_name_plural = "Categories"
 
     def __unicode__(self):
@@ -40,15 +43,16 @@ class Article(models.Model):
         return self.title
 
     class Meta:
-         permissions = (
-             ('group_view', 'Groups that can view an article'),
-         )
+        permissions = ( 
+            ('group_view', 'Groups that can view'),
+        ) 
 
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = uuslug(self.title, instance=self)
             self.title = self.title.encode("ascii", "ignore")
             self.created = datetime.datetime.now()
+            assign('djig.group_view', everybody_group, self)
         super(Article, self).save(*args, **kwargs)
 
     @models.permalink
@@ -64,6 +68,14 @@ class Article(models.Model):
         self.save()
         print "%s -- %s -- %s" % (self.calculated_score, hour_age, self.title)
         return (self.love_count-1) / pow((hour_age+2), gravity)
+
+    def get_allowed_groups(self):
+        allowed_groups = []
+        groups = get_groups_with_perms(self, attach_perms=True)
+        for group in groups.keys():
+            if "group_view" in groups[group]:
+                allowed_groups.append(group)
+        return allowed_groups
 
 class ArticleFormLean(ModelForm):
     class Meta:
--- djig/search_indexes.py
+++ djig/search_indexes.py
@@ -1,11 +1,16 @@
 import datetime
 from haystack import indexes
 from models import Article
+# from guardian.shortcuts import get_groups_with_perms
+
+from django.contrib.auth.models import Group
+everybody_group = Group.objects.get(name="Everybody")
 
 class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     text        = indexes.CharField(document=True, use_template=True)
     author      = indexes.CharField(model_attr='user')
     created     = indexes.DateTimeField(model_attr='created')
+    group_access= indexes.MultiValueField()
 
     def get_model(self):
         return Article
@@ -15,3 +20,11 @@ class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_updated_field(self):
         "created"
+
+    def prepare_group_access(self, obj):
+        return [everybody_group.pk]
+        allowed_groups = []
+        groups = Group.objects.filter(name='Everybody')
+        for group in groups:
+            allowed_groups.append(group.id)
+        return allowed_groups
